d16a15e4e6a440984c50408142c1417b10bb1912
eliAbergel <102365176+eliAbergel@users.noreply.github.com>
2022-05-04 09:07:37 -0400

    added bear

---
+++ Bear.java
@@ -0,0 +1,17 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class bear here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bear extends Actor
+{
+
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -11,11 +11,13 @@ public class MyWorld extends World
 
     /**
      * Constructor for objects of class MyWorld.
-     * 
+     * bear
      */
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        Bear bear = new Bear();
+        addObject(bear, 300, 200);
     }
 }
